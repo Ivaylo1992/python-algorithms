42a1daedd42d2cbbe76994bca11f7e0df9b521a9
Ivaylo1992 <ivaylop123@gmail.com>
2023-10-23 05:50:51 +0300

    insertion sort algorithm

---
+++ insertion_sort.py
@@ -0,0 +1,21 @@
+def insertion_sort(arr):
+    n = len(arr)
+
+    for i in range(1, n):
+        current_num = arr[i]
+
+        j = i - 1
+
+        while j >= 0 and current_num < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+
+        arr[j + 1] = current_num
+
+
+arr = [64, 12, 33, 55, 25, 8]
+
+print('Unsorted List:', arr )
+
+insertion_sort(arr)
+print('Sorted list:', arr)
